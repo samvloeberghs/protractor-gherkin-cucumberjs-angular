e28fedb5a8063f4ddc3b39b2d6998fa7b3d3898d
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2016-09-28 12:22:25 +0100

    fixed regex

--- src/app/email-validator/email.validator.ts
+++ src/app/email-validator/email.validator.ts
@@ -8,7 +8,7 @@ export class EmailValidator {
   static validEmail(control: FormControl): EmailValidatorFn {
     let value = control.value;
     /* tslint:disable */
-    const EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@']+(\.[^<>()\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@']+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     /* tslint:enable */
     if (value.length <= 5 || !EMAIL_REGEXP.test(control.value)) {
       return {

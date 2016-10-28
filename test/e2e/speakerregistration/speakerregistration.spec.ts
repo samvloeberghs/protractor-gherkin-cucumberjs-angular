511025d01b10dc25b8ce0d173d82738a8b8e1aea
Roman Gural <7.gural@gmail.com>
2016-10-28 19:42:24 +0200

    step two happy path

--- test/e2e/speakerregistration/speakerregistration.spec.ts
+++ test/e2e/speakerregistration/speakerregistration.spec.ts
@@ -8,38 +8,61 @@ import testData from './data';
 
 describe('speaker registration section', function () {
 
-    let stepOnePageObject = new StepOnePageObject();
-    let stepTwoPageObject = new StepTwoPageObject();
-    let stepThreePageObject = new StepThreePageObject();
+  let stepOnePageObject = new StepOnePageObject();
+  let stepTwoPageObject = new StepTwoPageObject();
+  let stepThreePageObject = new StepThreePageObject();
 
-    beforeEach(() => {
-        stepOnePageObject.goToStepOnePage();
+  beforeEach(() => {
+    stepOnePageObject.goToStepOnePage();
+  });
+
+  xit('should go to the first step of speaker registration', () => {
+    expect(stepOnePageObject.getCurrentUrl()).toEqual(browser.baseUrl + 'speaker-registration');
+    //expect(stepOnePageObject.getTitle()).toEqual('');
+  });
+
+  testData.forEach((test) => {
+
+    xit('should validate the form with ('
+      + test.input.name + ',' + test.input.name + ',' + test.input.description
+      + ') as ' + test.result.valid, () => {
+
+      stepOnePageObject.setName(test.input.name);
+      browser.sleep(1000);
+      stepOnePageObject.setEmail(test.input.email);
+      browser.sleep(1000);
+      stepOnePageObject.setDescription(test.input.description);
+      browser.sleep(1000);
+      stepOnePageObject.submitForm();
+      browser.sleep(1000);
+
+      expect(stepOnePageObject.formToBeValid()).toEqual(test.result.valid);
     });
+  });
 
-    it('should go to the first step of speaker registration', () => {
-        expect(stepOnePageObject.getCurrentUrl()).toEqual(browser.baseUrl + 'speaker-registration');
-        //expect(stepOnePageObject.getTitle()).toEqual('');
-    });
+  it('should go to the second step of speaker registration', () => {
+    stepOnePageObject.setName('Sam');
+    browser.sleep(1000);
+    stepOnePageObject.setEmail('sam@kwerri.be');
+    browser.sleep(1000);
+    stepOnePageObject.setDescription('Full stack dev');
+    browser.sleep(1000);
+    stepOnePageObject.submitForm();
+    browser.sleep(1000);
 
-    testData.forEach((test) => {
+    expect(stepOnePageObject.formToBeValid()).toEqual(true);
 
-        it('should validate the form with ('
-            + test.input.name + ',' + test.input.name + ',' + test.input.description
-            + ') as ' + test.result.valid, () => {
 
-            stepOnePageObject.setName(test.input.name);
-            browser.sleep(1000);
-            stepOnePageObject.setEmail(test.input.email);
-            browser.sleep(1000);
-            stepOnePageObject.setDescription(test.input.description);
-            browser.sleep(1000);
-            stepOnePageObject.submitForm();
-            browser.sleep(1000);
+    stepTwoPageObject.setSessionType('Speaker');
+    browser.sleep(1000);
+    stepTwoPageObject.setSessionTitle('title');
+    browser.sleep(1000);
+    stepTwoPageObject.setSessionDescription('description');
+    browser.sleep(1000);
+    stepTwoPageObject.submitForm();
+    browser.sleep(1000);
 
-            expect(stepOnePageObject.formToBeValid()).toEqual(test.result.valid);
-
-        });
-
-    });
+    expect(stepTwoPageObject.formToBeValid()).toEqual(true);
+  })
 
 });
--- test/e2e/speakerregistration/steptwo.page.ts
+++ test/e2e/speakerregistration/steptwo.page.ts
@@ -2,10 +2,41 @@ import { browser, element, by, protractor, ElementArrayFinder } from 'protractor
 import Promise = webdriver.promise.Promise;
 
 export class StepTwoPageObject {
+  private gotospeakerregistration = element(by.id('startspeakerregistrationprocess'));
 
+  private type = element(by.id('step-two-type'));
+  private title = element(by.id('step-two-title'));
+  private description = element(by.id('step-two-description'));
+  private stepTwoButtonSubmit = element(by.id('step-two-submit'));
 
-    constructor() {
+  constructor() {
 
-    }
-    
+  }
+
+  setSessionType(type: string) {
+    this.type.sendKeys(type);
+  }
+
+  setSessionTitle(title: string) {
+    this.title.sendKeys(title);
+  }
+
+  setSessionDescription(description: string) {
+    this.description.sendKeys(description);
+  }
+
+  submitForm() {
+    this.stepTwoButtonSubmit.click();
+  }
+
+  formToBeValid() {
+    return this.getAllErrorMessages().count().then(value => {
+      return value === 0;
+    });
+  }
+
+  private getAllErrorMessages(): ElementArrayFinder {
+    return element.all(by.css('.error-group'));
+  }
+
 }
